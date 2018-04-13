0ffe1f46e127d975bcfac9b00a8c6c22ca27d44c
wenweizww@wenweideMBP <zhou_wenwei@wuxiapptec.com>
2018-04-13 14:31:03 +0800

    reverser interger

--- leetcode/reverseInteger.py
+++ leetcode/reverseInteger.py
@@ -1,12 +1,32 @@
 
 
 def reverseInter(n):
+    print n
     if n < 0:
         n=(-1*n)
+        return "-"+reverserStr(str(n))
+    return reverserStr(str(n))
+def reverserStr(str1):
+    print str1[1:]
+    if len(str1)==1:
+        return str1
+    else:
+        return reverserStr(str1[1:len(str1)])+reverserStr(str1[0])
 
-    k = len(str(n))
+def reverseNonIter(n):
+    m=n
+    if n < 0:
+        m=n*-1;
+        return "-"+reverserNonIterNonNegativee(m)
 
-    for index  in range(0,n):
-        pow(10,index)
+    return
+def reverserNonIterNonNegativee(n):
+    k = ''
+    while n!=0:
+        m=n%10
+        n=n/10
+        k=k+str(m);
+    return k;
 
-reverseInter(8)
+# print reverseInter(8123456782)
+print reverserNonIterNonNegativee(9)
